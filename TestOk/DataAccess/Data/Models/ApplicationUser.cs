0e79370a53d5f6ad60f5112f3a0f8dd7290b15e5
TarasBat <taras,bat2000@gmail.com>
2020-03-01 19:22:35 +0200

    Fix merge bugs

---
+++ TestOk/DataAccess/Data/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+    }
+}
