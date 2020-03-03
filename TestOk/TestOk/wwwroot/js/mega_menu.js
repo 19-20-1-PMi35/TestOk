e36567617aeed360f596ec9c16ccb0a882e598f9
Yurii Horoshko <yuriy.horoshkolv@gmail.com>
2020-03-04 00:39:30 +0200

    Added menu item for tests, git ignore update, docker support

---
+++ TestOk/TestOk/wwwroot/js/mega_menu.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(() => {
+    $(".test-item").hover(function() {
+        $(this).css({"background-color": "rgba(86, 89, 227, 0.6)"});
+        $('html,body').css('cursor','pointer');
+    },
+    function(){
+        $(this).css("background-color", "white");
+        $('html,body').css('cursor','default');
+      });
+
+    $("#testsButton").hover(() => {
+        $(".tests-container").show();
+    }, () => {
+        $(".tests-container").hide();
+    });
+});
