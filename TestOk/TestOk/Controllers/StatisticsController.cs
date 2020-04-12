84f454cc0c0dc789e4cbcc63f31264146e729cf6
pasternakandrjj <pasternakandrjj@gmail.com>
2020-04-12 12:12:01 +0300

    starting commit

---
+++ TestOk/TestOk/Controllers/StatisticsController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using TestOk.Models;
+
+namespace TestOk.Controllers
+{
+    public class StatisticsController : SharedController
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ITestService _testService;
+
+        public StatisticsController(ILogger<HomeController> logger, ITestService testService) : base(testService)
+        {
+            _logger = logger;
+            _testService = testService;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
